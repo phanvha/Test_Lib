cf619405b7fb078300ce9ae8a6a7aec78c5cefa4
phanvha <iotlink2019@gmail.com>
2020-02-14 16:15:21 +0700

    V1.0.2

---
+++ awesome_library/src/main/java/com/map4d/awesome_library/SumNumber.java
@@ -0,0 +1,8 @@
+package com.map4d.awesome_library;
+
+public class SumNumber {
+    public static double getInstanceSum(double num1, double num2){
+        double Kq = num1 + num2;
+        return Kq;
+    }
+}
