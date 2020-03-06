27ab9c6f3de70b22b650bc3836c5af97a40a5304
phanvha <iotlink2019@gmail.com>
2020-03-06 14:59:33 +0700

    V1.0.5

--- awesome_library/src/main/java/com/map4d/awesome_library/Get_Smartcodes.java
+++ awesome_library/src/main/java/com/map4d/awesome_library/Get_Smartcodes.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import okhttp3.ResponseBody;
@@ -18,8 +19,8 @@ public class Get_Smartcodes {
     public static Context context;
 
     public static Model_Smartcode_Data model_smartcode_data;
-    public static JsonObject data;
-    public static Model_Smartcode_Data getDataSmartcode (Double latitude, Double longitude) {
+    public static JSONObject object;
+    public static JSONObject getDataSmartcode (Double latitude, Double longitude) {
 
         String latlng = latitude+","+longitude;
         API_Smartcode_Interface service = API_Smartcode.getClient2().create(API_Smartcode_Interface.class);
@@ -28,7 +29,84 @@ public class Get_Smartcodes {
             @Override
             public void onResponse(Call<Model_Smartcode_Data> call, Response<Model_Smartcode_Data> response) {
                 if (response.isSuccessful()){
-                    model_smartcode_data = response.body();
+
+                    //min
+                    JSONObject min = new JSONObject();
+                    try {
+                        min.put("lat", response.body().getResults().getMin().getLatitude());
+                        min.put("lng", response.body().getResults().getMin().getLongitude());
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //max
+                    JSONObject max = new JSONObject();
+                    try {
+                        max.put("lat", response.body().getResults().getMax().getLatitude());
+                        max.put("lng", response.body().getResults().getMax().getLongitude());
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //location
+                    JSONObject location = new JSONObject();
+                    try {
+                        location.put("lat", response.body().getResults().getLocation().getLatitude());
+                        location.put("lng", response.body().getResults().getLocation().getLongitude());
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //compoundCode
+                    JSONObject compoundCode = new JSONObject();
+                    try {
+                        compoundCode.put("compoundCode", response.body().getResults().getCompoundCode());
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //smartcodes
+                    JSONObject smartcodes = new JSONObject();
+                    try {
+                        smartcodes.put("smartCode", response.body().getResults().getSmartCode());
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //center
+                    JSONObject center = new JSONObject();
+                    try {
+                        center.put("lat", response.body().getResults().getCenter().getLatitude());
+                        center.put("lng", response.body().getResults().getCenter().getLongitude());
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+
+                    //result
+                    JSONObject result = new JSONObject();
+                    try {
+                        result.put("min", min);
+                        result.put("max", max);
+                        result.put("location", location);
+                        result.put("compoundCode", compoundCode);
+                        result.put("smartCode", smartcodes);
+                        result.put("center", center);
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    //
+                    object = new JSONObject();
+                    try {
+                        object.put("code", response.body().getCode());
+                        object.put("result", result);
+
+                    } catch (JSONException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    Log.e("jsonObject", object.toString());
                 }
             }
 
@@ -37,7 +115,7 @@ public class Get_Smartcodes {
 
             }
         });
-        return model_smartcode_data;
+        return object;
     }
     public static int x(int a, int b){
         return a+b;
